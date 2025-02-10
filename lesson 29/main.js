91e7152d04df1c59a19ba9259808245a539908c8
PPACAA406 <pacolliorges02@gmail.com>
2025-02-10 02:51:13 -0800

    .

---
+++ lesson 29/main.js
@@ -0,0 +1,32 @@
+var programmingLanguages = ['JavaScript', 'PHP','Python'];
+
+console.log(programmingLanguages);
+
+console.log(programmingLanguages[0]);
+
+programmingLanguages.push('Java');
+console.log(programmingLanguages)
+
+programmingLanguages.pop();
+console.log(programmingLanguages);
+
+
+programmingLanguages.unshift('C#');
+console.log(programmingLanguages);
+
+programmingLanguages.shift();
+console.log(programmingLanguages);
+
+programmingLanguages.splice(0,2, 'Ruby');
+console.log(programmingLanguages);
+
+console.log(Math.random()*5);
+console.log(Math.floor(Math.random()*5));
+
+var students=['Anid','Reald']
+
+var[s1,s2]=students;
+
+console.log(s1);
+console.log(s2);
+
